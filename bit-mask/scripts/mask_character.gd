df312770378ff28d7e8e116263a04004a4849b6f
Brian Ingles <github@emeraldwalk.com>
2026-02-01 16:23:03 -0600

    Basic scene and characters layout

--- bit-mask/scripts/mask_character.gd
+++ bit-mask/scripts/mask_character.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = %Sprite
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 140.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction.x:
+		velocity.x = direction.x * SPEED
+		sprite.flip_h = direction.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction.y:
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if velocity.length():
+		if not sprite.is_playing():
+			print("play")
+			sprite.play("walk")
+	elif sprite.is_playing():
+		print("stop")
+		sprite.stop()
+		sprite.frame = 1
 
 	move_and_slide()
